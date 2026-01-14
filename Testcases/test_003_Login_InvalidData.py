8ea6da319821846e9aa72bd9a20b196cc9145f81
subupkrl <139966058+subupkrl@users.noreply.github.com>
2026-01-14 14:16:23 +0545

    Enhanced code

--- Testcases/test_003_Login_InvalidData.py
+++ Testcases/test_003_Login_InvalidData.py
@@ -1,4 +1,4 @@
-from Base.InitiateDriver import startBrowser,closeBrowser
+
 from selenium.webdriver import Chrome
 
 def test_InvalidLogin():
@@ -8,8 +8,7 @@ def test_InvalidLogin():
     driver.find_element('xpath',"//input[@name='email']").send_keys("1hello")
     driver.find_element('xpath',"//input[@name='pass']").send_keys("4567")
     driver.find_element('xpath',"//button[@name='login']").click()
-    
-    closeBrowser()
+    driver.close()
     
     
     
