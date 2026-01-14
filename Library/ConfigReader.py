a2ad597f7327e993b426bb4eb13f02198b4206b8
subupkrl <139966058+subupkrl@users.noreply.github.com>
2026-01-14 14:08:01 +0545

    Enhanced code

--- Library/ConfigReader.py
+++ Library/ConfigReader.py
@@ -1,17 +1,31 @@
+# import configparser
+
+# def configRead(section,key):
+    
+#     config = configparser.ConfigParser()
+#     config.read('../ConfigurationFiles/Config.cfg')
+    
+#     return config.get(section,key)
+
+# # print(configRead('Details','APP_URL'))
+
+# def readElements(section,key):
+    
+#     config = configparser.ConfigParser()
+#     config.read("../ConfigurationFiles/Elements.cfg")
+    
+#     return config.get(section,key)
+
 import configparser
+import os
 
-def configRead(section,key):
-    
-    config = configparser.ConfigParser()
-    config.read('../ConfigurationFiles/Config.cfg')
-    
-    return config.get(section,key)
+config = configparser.ConfigParser()
 
-# print(configRead('Details','APP_URL'))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def readElements(section,key):
-    
-    config = configparser.ConfigParser()
-    config.read("../ConfigurationFiles/Elements.cfg")
-    
-    return config.get(section,key)
+config_path = os.path.join(BASE_DIR, "Config", "config.ini")
+
+config.read(config_path)
+
+def configRead(section, key):
+    return config.get(section, key)
